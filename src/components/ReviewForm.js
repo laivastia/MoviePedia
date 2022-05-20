9d634b2ee1f167063c22ec2786c831fa01db27bc
Vastia <zmfwl123@gmail.com>
2022-05-20 14:49:09 +0900

    파일 첨부하기
    input:type=file 폼 생성
    비제어 컴포넌트 생성
    함수 추상화

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
+import FileInput from "./FileInput";
 
 function ReviewForm() {
   const [value, setValue] = useState({
     title: "",
     rating: 0,
     content: "",
+    imgFile: null,
   });
 
-  const handleChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
+    handleChange(name, value);
+  };
+
+  const handleChange = (name, value) => {
     setValue((prevValues) => ({
       ...prevValues,
       [name]: value,
@@ -22,19 +28,28 @@ function ReviewForm() {
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
-      <input name="title" value={value.title} onChange={handleChange}></input>
+      <FileInput
+        name="imgFile"
+        value={value.imgFile}
+        onChange={handleInputChange}
+      />
+      <input
+        name="title"
+        value={value.title}
+        onChange={handleInputChange}
+      ></input>
       <input
         type="number"
         max={5}
         min={1}
         name="rating"
         value={value.rating}
-        onChange={handleChange}
+        onChange={handleInputChange}
       ></input>
       <textarea
         name="content"
         value={value.content}
-        onChange={handleChange}
+        onChange={handleInputChange}
       ></textarea>
       <button type="submit">확인</button>
     </form>
