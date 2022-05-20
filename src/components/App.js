83300f131060c0fe6789d53cf89383f4cdbcd87a
Vastia <zmfwl123@gmail.com>
2022-05-20 11:13:55 +0900

    API연동...
    index 파일 수정
    api 서버 연동
    음식 목록 출력
    정렬 기능 추가
    더 보기 기능 추가
    에러 처리
    조건부 랜더링 조건 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,70 @@
-import items from "../mock.json";
+import { useEffect, useState } from "react";
+import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
 
+const LIMIT = 6;
+
 function App() {
+  const [items, setItems] = useState([]);
+  const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLodingError] = useState(null);
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
+  const handleLoad = async (options) => {
+    let result;
+    try {
+      setIsLoading(true);
+      setLodingError(null);
+      result = await getReviews(options);
+    } catch (error) {
+      setLodingError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+    const { reviews, paging } = result;
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems((prevItems) => [...prevItems, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
+  };
+
+  useEffect(() => {
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  }, [order]);
+
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더 보기
+        </button>
+      )}
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </div>
   );
 }
